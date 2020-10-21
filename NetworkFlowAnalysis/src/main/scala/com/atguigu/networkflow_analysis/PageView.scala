87a981276688fcc6fc4025171da80b4d7ded0524
liuyang <liuyang@coohua.com>
2020-10-21 11:43:41 +0800

    网站PV统计

---
+++ NetworkFlowAnalysis/src/main/scala/com/atguigu/networkflow_analysis/PageView.scala
@@ -0,0 +1,82 @@
+package com.atguigu.networkflow_analysis
+
+import java.lang
+
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+// 定义输入数据样例类
+case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long)
+
+case class PvCount(windowEnd: Long, count: Long)
+
+object PageView {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.setParallelism(1)
+
+    val resource = getClass.getResource("/UserBehavior.csv")
+    val inputStream = env.readTextFile(resource.getPath)
+
+    val dataStream: DataStream[UserBehavior] = inputStream
+      .map(data => {
+        val arr = data.split(",")
+        UserBehavior(arr(0).toLong, arr(1).toLong, arr(2).toInt, arr(3), arr(4).toLong)
+      })
+      .assignAscendingTimestamps(_.timestamp * 1000L)
+
+    val pvStream = dataStream
+      .filter(_.behavior == "pv")
+      .map(data => ("pv", 1L)) // 定义个pv字符串作为分组的dummy key
+      .keyBy(_._1) // 所有数据分到一个组
+      .timeWindow(Time.hours(1))
+      .aggregate(new PvCountAgg(), new PvCountWindowResult())
+
+    pvStream.print()
+
+    env.execute()
+  }
+}
+
+class PvCountAgg() extends AggregateFunction[(String, Long), Long, Long] {
+  override def createAccumulator(): Long = 0L
+
+  override def add(in: (String, Long), acc: Long): Long = acc + 1
+
+  override def getResult(acc: Long): Long = acc
+
+  override def merge(acc: Long, acc1: Long): Long = acc + acc1
+}
+
+class PvCountWindowResult() extends WindowFunction[Long, PvCount, String, TimeWindow] {
+  override def apply(key: String, window: TimeWindow, input: Iterable[Long], out: Collector[PvCount]): Unit = {
+    out.collect(PvCount(window.getEnd, input.head))
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
