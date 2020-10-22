e6f84acf04f9fc2fb4fe92b90b6b4a90a050854b
liuyang <liuyang@coohua.com>
2020-10-22 09:47:54 +0800

    网站PV统计-并行度优化和处理数据倾斜

--- NetworkFlowAnalysis/src/main/scala/com/atguigu/networkflow_analysis/PageView.scala
+++ NetworkFlowAnalysis/src/main/scala/com/atguigu/networkflow_analysis/PageView.scala
@@ -1,14 +1,16 @@
 package com.atguigu.networkflow_analysis
 
-import java.lang
-
-import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.functions.{AggregateFunction, MapFunction}
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
+
+import scala.util.Random
 
 // 定义输入数据样例类
 case class UserBehavior(userId: Long, itemId: Long, categoryId: Int, behavior: String, timestamp: Long)
@@ -19,7 +21,7 @@ object PageView {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    env.setParallelism(1)
+    //    env.setParallelism(1)
 
     val resource = getClass.getResource("/UserBehavior.csv")
     val inputStream = env.readTextFile(resource.getPath)
@@ -33,12 +35,19 @@ object PageView {
 
     val pvStream = dataStream
       .filter(_.behavior == "pv")
-      .map(data => ("pv", 1L)) // 定义个pv字符串作为分组的dummy key
+      //      .map(data => ("pv", 1L)) // 定义个pv字符串作为分组的dummy key
+      .map(new MyMapper())
       .keyBy(_._1) // 所有数据分到一个组
       .timeWindow(Time.hours(1))
       .aggregate(new PvCountAgg(), new PvCountWindowResult())
 
-    pvStream.print()
+    val totalPvStream = pvStream
+      .keyBy(_.windowEnd)
+      .process(new TotalPvCountResult())
+
+    totalPvStream.print()
+
+    //    pvStream.print()
 
     env.execute()
   }
@@ -57,6 +66,35 @@ class PvCountAgg() extends AggregateFunction[(String, Long), Long, Long] {
 class PvCountWindowResult() extends WindowFunction[Long, PvCount, String, TimeWindow] {
   override def apply(key: String, window: TimeWindow, input: Iterable[Long], out: Collector[PvCount]): Unit = {
     out.collect(PvCount(window.getEnd, input.head))
+  }
+}
+
+// 自定义Mapper 随机生成分组的key
+class MyMapper() extends MapFunction[UserBehavior, (String, Long)] {
+  override def map(t: UserBehavior): (String, Long) = {
+    (Random.nextString(10), 1L)
+  }
+}
+
+class TotalPvCountResult() extends KeyedProcessFunction[Long, PvCount, PvCount] {
+
+  // 定义一个状态 保存所有count总和
+
+  lazy val totalPvCountResultState: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("total-pv", classOf[Long]))
+
+  override def processElement(i: PvCount, context: KeyedProcessFunction[Long, PvCount, PvCount]#Context, collector: Collector[PvCount]): Unit = {
+    // 每来一个数据 将count值叠加在当前状态上
+    val currentTotalCount = totalPvCountResultState.value()
+    totalPvCountResultState.update(currentTotalCount + i.count)
+
+    // 注册定时器 windowEnd+1ms
+    context.timerService().registerEventTimeTimer(i.windowEnd + 1)
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, PvCount, PvCount]#OnTimerContext, out: Collector[PvCount]): Unit = {
+    val totalPvCount = totalPvCountResultState.value()
+    out.collect(PvCount(ctx.getCurrentKey, totalPvCount))
+    totalPvCountResultState.clear()
   }
 }
 
