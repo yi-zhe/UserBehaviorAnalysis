b3038a292a4ad057231d6fa25a0b9a1b45fea5a3
liuyang <liuyang@coohua.com>
2020-10-26 21:53:14 +0800

    广告点击统计基本实现

---
+++ MarketAnalysis/src/main/scala/com/atguigu/market_analysis/AdClickAnalysis.scala
@@ -0,0 +1,57 @@
+package com.atguigu.market_analysis
+
+import java.sql.Timestamp
+
+import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+case class AdClickLog(userId: Long, adId: Long, province: String, city: String, timestamp: Long)
+
+case class AdClickCountByProvince(windowEnd: String, province: String, count: Long)
+
+object AdClickAnalysis {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val resource = getClass.getResource("/AdClickLog.csv")
+    val inputStream: DataStream[String] = env.readTextFile(resource.getPath)
+
+    val adLogStream: DataStream[AdClickLog] = inputStream
+      .map(data => {
+        val arr = data.split(",")
+        AdClickLog(arr(0).toLong, arr(1).toLong, arr(2), arr(3), arr(4).toLong)
+      })
+      .assignAscendingTimestamps(_.timestamp * 1000L)
+
+    val adCountResultStream = adLogStream
+      .keyBy(_.province)
+      .timeWindow(Time.days(1), Time.seconds(5))
+      .aggregate(new AdCountAgg(), new AdCountWindowResult())
+    adCountResultStream.print()
+    env.execute()
+  }
+}
+
+class AdCountAgg() extends AggregateFunction[AdClickLog, Long, Long] {
+  override def createAccumulator(): Long = 0L
+
+  override def add(in: AdClickLog, acc: Long): Long = acc + 1
+
+  override def getResult(acc: Long): Long = acc
+
+  override def merge(acc: Long, acc1: Long): Long = acc + acc1
+}
+
+class AdCountWindowResult() extends WindowFunction[Long, AdClickCountByProvince, String, TimeWindow] {
+  override def apply(key: String, window: TimeWindow, input: Iterable[Long], out: Collector[AdClickCountByProvince]): Unit = {
+    val end = new Timestamp(window.getEnd).toString
+    out.collect(AdClickCountByProvince(end, key, input.head))
+  }
+}
